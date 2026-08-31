01a484faa4d8c56eac79a258fd5dd461bad2548e
[Daniel Stein] <danieljuliusstein@gmail.com>
2026-08-31 16:30:10 -0400

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render (
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />
+  </React.StrictMode>
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
