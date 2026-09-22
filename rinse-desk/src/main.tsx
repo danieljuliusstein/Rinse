cbc040a0354a108f155a5af934b7cae93aa22d8d
[Daniel Stein] <danieljuliusstein@gmail.com>
2026-09-22 14:53:32 -0400

    some changes

--- rinse-desk/src/main.tsx
+++ rinse-desk/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './i18n'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
     <SpeedInsights />
   </React.StrictMode>,
 )
